3d6f6d50b5aac3303d41053ca3bbc21e537b59ce
MuditPanwar <153761879+MuditPanwar@users.noreply.github.com>
2025-07-18 15:05:07 +0530

    performing Text Classification

--- Dashboard.py
+++ Dashboard.py
@@ -1,1 +1,24 @@
-#Creating Dashboard for our Book Recommender
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import TextLoader
+
+from langchain.embeddings import HuggingFaceEmbeddings
+embedding_model = HuggingFaceEmbeddings(model_name= "sentence-transformers/all-MiniLM-L6-v2")
+
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_chroma import Chroma
+
+import gradio as gr
+
+load_dotenv()
+
+books = pd.read_csv("books_with_emotions.csv.csv")
+books["large_thumbnail"] = books["thumbnail"] + "&fife=w800"
+books["large_thumbnail"] = np.where(
+    books["large_thumbnail"].isna(),
+    "cover_not_found.jpg",
+    books["large_thumbnail"]
+)
+
