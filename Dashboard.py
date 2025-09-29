6c7b16ed28768fee01e84b87bfc326d6e4856336
MuditPanwar <153761879+MuditPanwar@users.noreply.github.com>
2025-09-29 10:48:16 +0530

    Dashboard updation

--- Dashboard.py
+++ Dashboard.py
@@ -29,7 +29,38 @@ db_books = Chroma.from_documents(documents, embedding_model)
 
 def retrieve_semantic_recommendations(
         query: str,
-        category: str = None
+        category: str = None,
+        tone: str =None,
+        initial_top_k: int = 50,
+        final_top_k: int = 16,
+) -> pd.DataFrame:
+    recs = db_books.similarity_search_with_score(query, k=initial_top_k)
+    books_list = [int(rec.page_content.strip('').split()[0])for rec in recs]
+    book_recs = books[books["isbn13"].isin(books_list)].head(final_top_k)
 
-)
+    if category != "All":
+        book_recs = book_recs[book_recs["category"] == category][:final_top_k]
+    else:
+        book_recs = book_recs.head(final_top_k)
+
+    if tone == "Happy":
+        book_recs.sort_values(by="joy", ascending=False, inplace=True)
+    elif tone == "Surprising":
+        book_recs.sort_values(by="Surprise", ascending=False, inplace=True)
+    elif tone == "Angry":
+        book_recs.sort_values(by="Anger", ascending=False, inplace=True)
+    elif tone == "Suspenseful":
+        book_recs.sort_values(by="Fear", ascending=False, inplace=True)
+    elif tone == "Sad":
+        book_recs.sort_values(by="Sadness", ascending=False, inplace=True)
+
+    return book_recs
+
+def recommend_books(
+        query: str,
+        category: str,
+        tone: str
+):
+
+    recommendations
 
